db77473c5f299e73d810838015886defb645c84a
Liubomyr680 <lshynkaruk8@gmail.com>
2020-03-05 10:06:16 +0200

    Dependency Injection with Spring

---
+++ src/main/java/liubomyr/springframework/DependencyInjection/DependencyInjectionExamplesApplication.java
@@ -0,0 +1,37 @@
+package liubomyr.springframework.DependencyInjection;
+
+import liubomyr.springframework.DependencyInjection.controllers.ConstructorInjectedController;
+import liubomyr.springframework.DependencyInjection.controllers.MyController;
+import liubomyr.springframework.DependencyInjection.controllers.PropertyInjectedController;
+import liubomyr.springframework.DependencyInjection.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DependencyInjectionExamplesApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(DependencyInjectionExamplesApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+
+		System.out.println("------ Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("--------- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("-------- Constructor" );
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+}
